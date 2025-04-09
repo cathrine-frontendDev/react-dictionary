6489640db7287dfef2f65390e05d4c06d0764850
Cathrine <cathrinekamasho@gmail.com>
2025-04-09 15:17:38 +0200

    updated the Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -16,9 +16,9 @@ export default function Dictionary () {
         event.preventDefault();
         
 
-    let apiUrl ="https://api.shecodes.io/dictionary/v1/define?word=${word}&key={apiKey}";
     let apiKey =" 0eb691o00e3fb24a210b62tf2c42a9e3";
-    let word= keyWord;
+    let apiUrl ="https://api.shecodes.io/dictionary/v1/define?word=${book}&key=${0eb691o00e3fb24a210b62tf2c42a9e3}";
+    let word= book;
     axios.get(apiUrl).then(handleResponse);
     }
 
