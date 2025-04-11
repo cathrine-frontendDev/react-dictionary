1c9edd28a40398ac1df989bb7ddae61a1a5a0f03
Cathrine <cathrinekamasho@gmail.com>
2025-04-11 13:47:01 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -16,7 +16,7 @@ export default function Dictionary () {
         event.preventDefault();
         
 
-    let apiKey =" 0eb691o00e3fb24a210b62tf2c42a9e3";
+    let apiKey ="0eb691o00e3fb24a210b62tf2c42a9e3";
     let apiUrl ='https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}';
     axios.get(apiUrl).then(handleResponse);
     }
